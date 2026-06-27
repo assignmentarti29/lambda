88706de19d08520898799f859875e480c4f933ad
NileshMungase <nileshmungase101@gmail.com>
2026-06-27 11:49:57 +0530

    updated file

--- src/main/java/com/javaexpress/Lambda.java
+++ src/main/java/com/javaexpress/Lambda.java
@@ -8,9 +8,10 @@ public class Lambda {
 
         public static void main(String[] args) {
 
-            List<String> names = Arrays.asList("Arati", "Rahul", "Neha");
+            List<String> names = Arrays.asList("Arati", "Rahul", "Neha","Nilesh");
 
             names.forEach(name -> System.out.println(name));
+            System.out.println("Hey Arati");
         }
     }
 
